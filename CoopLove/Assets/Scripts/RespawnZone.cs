93064d6dcc33da1252b1366ad76ed3b9405fdd6c
Andreas Roither <andi.roither@protonmail.com>
2019-03-28 15:37:57 +0100

    Update RespawnZone.cs

--- CoopLove/Assets/Scripts/RespawnZone.cs
+++ CoopLove/Assets/Scripts/RespawnZone.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class RespawnZone : MonoBehaviour
 {
+    public float maxVelocity = 1f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         float x = (float)GetRandomNumber(-8d, 8d);
         collision.gameObject.transform.position = new Vector3(x,7,0);
+
+        Rigidbody2D rigidbody2D = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        if (rigidbody2D != null)
+        {
+            if (Mathf.Abs(rigidbody2D.velocity.x) > maxVelocity || Mathf.Abs(rigidbody2D.velocity.y) > maxVelocity)
+                rigidbody2D.velocity = Vector2.ClampMagnitude(rigidbody2D.velocity, maxVelocity);
+        }
 
         Platform platform = collision.gameObject.GetComponent<Platform>();
 
