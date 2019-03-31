8f87b62a6336d969fdc27091868933c1a2f0be7e
Andreas Roither <andi.roither@protonmail.com>
2019-03-31 18:50:24 +0200

    CoopLoveServer + Client Scripts
    Setting up a project for the CoopLoveServer and added initial scripts

---
+++ CoopLove/Assets/Scripts/Client.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Client : MonoBehaviour
+{
+    private bool isRunning = false;
+    private byte reliableChanel;
+    private int hostId;
+    private const string SERVER_IP = "127.0.0.1";
+
+    private const int MAX_USER = 100;
+    private int WEB_PORT = 26001;
+    private const int PORT = 26000;
+    private byte error;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        Init();
+    }
+
+    /// <summary>
+    /// Initialize the Server
+    /// Set Channels and topology
+    /// Add Hosts, WebSocket
+    /// </summary>
+    private void Init()
+    {
+        NetworkTransport.Init();
+
+        ConnectionConfig cc = new ConnectionConfig();
+        reliableChanel = cc.AddChannel(QosType.Reliable);
+
+        HostTopology topology = new HostTopology(cc, MAX_USER);
+
+        hostId = NetworkTransport.AddHost(topology, 0);
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+        
+        // web client
+        NetworkTransport.Connect(hostId, SERVER_IP, WEB_PORT, 0, out error);  
+#else
+        // standalone client
+        NetworkTransport.Connect(hostId, SERVER_IP, PORT, 0, out error);
+#endif
+
+        isRunning = true;
+    }
+
+    /// <summary>
+    /// Shutdown server
+    /// </summary>
+    private void Shutdown()
+    {
+        isRunning = false;
+        NetworkTransport.Shutdown();
+    }
+}
---
+++ CoopLoveServer/Assets/Scripts/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+    private bool isRunning = false;
+    private byte reliableChanel;
+    private int hostId;
+    private int webHostId;
+
+    private const int MAX_USER = 100;
+    private int WEB_PORT = 26001;
+    private const int PORT = 26000;
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        Init();
+    }
+
+    /// <summary>
+    /// Initialize the Server
+    /// Set Channels and topology
+    /// Add Hosts, WebSocket
+    /// </summary>
+    private void Init()
+    {
+        NetworkTransport.Init();
+
+        ConnectionConfig cc = new ConnectionConfig();
+        reliableChanel = cc.AddChannel(QosType.Reliable);
+
+        HostTopology topology = new HostTopology(cc, MAX_USER);
+
+        hostId = NetworkTransport.AddHost(topology, PORT, null);
+        webHostId = NetworkTransport.AddWebsocketHost(topology, WEB_PORT, null);
+
+        Debug.Log($"> Started Server\nOpened Connection on port {PORT} and webport {WEB_PORT}");
+        isRunning = true;
+    }
+
+    /// <summary>
+    /// Shutdown server
+    /// </summary>
+    private void Shutdown()
+    {
+        isRunning = false;
+        NetworkTransport.Shutdown();
+    }
+}
